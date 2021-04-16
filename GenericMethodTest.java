359bf96a1a7c543be4a4688db402ea78c8a8b8b7
WNDH-VD3m <77713690+WNDH-VD3m@users.noreply.github.com>
2021-04-16 22:22:31 +0800

    Add files via upload

--- GenericMethodTest.java
+++ GenericMethodTest.java
@@ -1,22 +1,20 @@
-package test5;
+package test11;
 
 public class GenericMethodTest {
-
     public static <E> void printArray(E[] inputArray){
-
-        for(E element : inputArray){
-            System.out.printf("%s",element);
+        for (E element : inputArray){
+            System.out.printf("%s " , element);
         }
+        System.out.println();
     }
 
-    public static void main(String args[]){
-        Integer[] intArray = {1,2,3,4};
-        Double[] doubleArray = {3.1, 2.3, 1.2};
+    public static void main(String[] args){
+        Integer[] intArray = {1,2,3,4,5};
+        Double[] doubleArray = {1.2,3.2,5.7,5.9};
         Character[] charArray = {'H','E','L','L','O'};
-        System.out.println("整型数组元素为:" );
-        printArray(intArray);
 
-        System.out.println("\n字符型数组元素为:");
+        printArray(intArray);
+        printArray(doubleArray);
         printArray(charArray);
     }
 
--- MaximumTest.java
+++ MaximumTest.java
@@ -1,21 +1,29 @@
-package test5;
+/*
+* 有界的类型参数:
+*
+* 可能有时候，你会想限制那些被允许传递到一个类型参数的类型种类范围。
+* 例如，一个操作数字的方法可能只希望接受Number或者Number子类的实例。这就是有界类型参数的目的。
+*
+* 要声明一个有界的类型参数，首先列出类型参数的名称，后跟extends关键字，最后紧跟它的上界。
+*
+* */
+
+
+package test11;
 
 public class MaximumTest {
-
-    // 比较三个值并返回最大值
-    public static <T extends Comparable<T>> T maximum(T x, T y, T z)
-    {
-        T max = x; // 假设x是初始最大值
-        if ( y.compareTo( max ) > 0 ){
-            max = y; //y 更大
+    public static <E extends Comparable<E>> E maximum(E x, E y, E z){
+        E max = x;
+        if (y.compareTo(max) > 0){
+            max = y;
         }
-        if ( z.compareTo( max ) > 0 ){
-            max = z; // 现在 z 更大
+        if (z.compareTo(max) > 0){
+            max = z;
         }
-        return max; // 返回最大对象
+        return max;
     }
-    public static void main( String args[] )
-    {
+
+    public static void main( String args[] ){
         System.out.printf( "%d, %d 和 %d 中最大的数为 %d\n\n",
                 3, 4, 5, maximum( 3, 4, 5 ) );
 
@@ -25,5 +33,5 @@ public class MaximumTest {
         System.out.printf( "%s, %s 和 %s 中最大的数为 %s\n","pear",
                 "apple", "orange", maximum( "pear", "apple", "orange" ) );
     }
-    
+
 }
